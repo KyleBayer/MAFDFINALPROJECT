02288c3b31fc60ad86341d4c449c95d57ee3faac
Joree Anthony Miranda <joreeanthony.miranda@dcmail.ca>
2018-04-20 02:08:35 -0400

    absolutely done #2 and #3

--- EDITS/DataSplitCount/DataSplitCount.cbl
+++ EDITS/DataSplitCount/DataSplitCount.cbl
@@ -1,5 +1,10 @@
        identification division.
        program-id. DataSplitCount.
+       author. Joree Miranda, Kyle Bayer, Ashante Smith
+       date-written. 2018-04-20
+      *This is our 2nd program for our MAFD final project. It will split
+      *returns into one file, sales and layaways into another file.
+      *It will then count and calculate totals.
 
        environment division.
        configuration section.
@@ -14,13 +19,16 @@
 
       * output file declarations
            select sale-layaways-file
-           assign to "../../../data/salelayaways.out"
+           assign to "../../../data/salelayaways.dat"
            organization is line sequential.
 
            select returns-file
-           assign to "../../../data/returns.out"
+           assign to "../../../data/returns.dat"
            organization is line sequential. 
 
+           select report-file
+           assign to "../../../data/Prog2Report.out"
+           organization is line sequential.
        
 
        data division.
@@ -31,74 +39,124 @@
            record contains 36 characters.
 
        01 input-line.
-           05 il-transaction-code      pic X.
-           05 il-transaction-amount    pic 9(5)V99.
-           05 il-payment-type          pic XX.
-           05 il-store-number          pic XX.
-           05 il-invoice-number        pic X(9).
-           05 il-sku-code              pic X(15).
+           05 il-transaction-code         pic X.
+           05 il-transaction-amount       pic 9(5)V99.
+           05 il-payment-type             pic XX.
+           05 il-store-number             pic XX.
+           05 il-invoice-number           pic X(9).
+           05 il-sku-code                 pic X(15).
 
        fd sale-layaways-file
            data record is sale-layaways-line
            record contains 36 characters.
 
-       01 sale-layaways-line           pic x(36).
+       01 sale-layaways-line              pic x(36).
 
        fd returns-file
            data record is returns-line
            record contains 36 characters.
 
-       01 returns-line                 pic x(36).
+       01 returns-line                    pic x(36).
+
+       fd report-file
+           data record is report-line
+           record contains 36 characters.
+
+       01 report-line                      pic x(36).
 
        working-storage section.
 
-       01 ws-transaction-code          pic x
+       01 ws-transaction-code             pic x
            value spaces.
 
+       01 ws-layaways-amount              pic 9(6)v99
+           value 0.
+
+       01 ws-sales-amount                 pic 9(6)v99
+           value 0.
+
+       01 ws-returns-amount               pic 9(6)v99
+           value 0.
+
+       01 ws-grand-total                  pic 9(6)v99
+           value 0.
+
+       01 ws-total-amount                 pic 9(6)v99
+           value 0.
+
+       01 ws-total-layaways-amount-detail.
+           05 filler                      pic x(24)
+               value "TOTAL LAYAWAYS AMOUNT: ".
+           05 ws-layaways-amount-output   pic $z9,999.99
+               value 0.
+
+       01 ws-total-sale-amount-detail.
+           05 filler                      pic x(24)
+               value "TOTAL SALES AMOUNT: ".
+           05 ws-sales-amount-output      pic $99,999.99
+               value 0.
+
+       01 ws-total-amount-detail.
+           05 filler                      pic x(24)
+               value "TOTAL S&L AMOUNT: ".
+           05 ws-total-amount-output      pic $99,999.99
+               value 0.
+
+       01 ws-total-return-amount-detail.
+           05 filler                      pic x(24)
+               value "TOTAL RETURNS AMOUNT: ".
+           05 ws-returns-amount-output    pic $zz,999.99
+               value 0.
+
+       01 ws-grand-total-detail.
+           05 filler                      pic x(24)
+               value "GRAND TOTAL: ".
+           05 ws-grand-total-output       pic $zz,999.99
+
        01 ws-total-sale-layaways-detail.
-           05 filler                   pic x(24)
-               value "Total Sales & Layaways: ".
-           05 ws-total-sale-layaways   pic 9(3)
+           05 filler                      pic x(24)
+               value "TOTAL SALES & LAYAWAYS: ".
+           05 ws-total-sale-layaways      pic 9(3)
                value 0.
 
        01 ws-total-returns-detail.           
-           05 filler                   pic x(25)
-               value "Total Returns: ".
-           05 ws-total-returns         pic 9(2)
+           05 filler                      pic x(25)
+               value "TOTAL RETURNS: ".
+           05 ws-total-returns            pic 9(2)
                value 0.
 
        01 ws-total-sale-detail.
-           05 filler                   pic x(25)
-               value "Total Sales: ".
-           05 ws-total-sales           pic 9(2)
+           05 filler                      pic x(25)
+               value "TOTAL SALES: ".
+           05 ws-total-sales              pic 9(2)
                value 0.
 
        01 ws-total-layaways-detail.
-           05 filler                   pic x(25)
-               value "Total Layaways: ".
-           05 ws-total-layaways        pic 9(2)
+           05 filler                      pic x(25)
+               value "TOTAL LAYAWAYS: ".
+           05 ws-total-layaways           pic 9(2)
                value 0.
 
-       01 ws-grand-total-detail.              
-           05 filler                   pic x(24)
-               value "Grand Total: ".
-           05 ws-grand-total           pic 9(3)
+       01 ws-transaction-total-detail.              
+           05 filler                      pic x(24)
+               value "TOTAL TRANSACTIONS: ".
+           05 ws-total-transactions       pic 9(3)
                value 0.
 
        01 ws-sales-percentage-detail.
-           05 filler                   pic x(24)
-               value "Sales Percentage: ".
-           05 ws-sales-percentage      pic V99
+           05 filler                      pic x(24)
+               value "SALES PERCENTAGE: ".
+           05 ws-sales-percentage         pic V99
                value 0.
-           05 filler                   pic X
+           05 filler                      pic X
                value "%".
 
        01 ws-layaways-percentage-detail.
-           05 filler                   pic x(24)
-               value "Layaways Percentage: ".
-           05 ws-layaways-percentage   pic V99
+           05 filler                      pic x(24)
+               value "LAYAWAYS PERCENTAGE: ".
+           05 ws-layaways-percentage      pic V99
                value 0.
-           05 filler                   pic X
+           05 filler                      pic X
                value "%".
 
 
@@ -109,20 +167,20 @@
        01 ws-transaction-code-flag     pic x
            value "N".
 
-       77 ws-sale-transaction-code     pic x
+       77 ws-sale-code                 pic x
            value "S".
 
-       77 ws-layaway-transaction-code  pic x
+       77 ws-layaway-code              pic x
            value "L".
 
-       77 ws-returns-transaction-code  pic x
+       77 ws-return-code              pic x
            value "R".
 
 
        procedure division.
       * open files
            open input input-file.
-           open output sale-layaways-file returns-file.
+           open output sale-layaways-file returns-file report-file.
        
       * read initial record from input-file
            read input-file at end move "Y" to ws-eof-flag.
@@ -132,7 +190,7 @@
            end-perform.
            
            perform 40-output-totals.
-           close input-file sale-layaways-file returns-file.
+           close input-file sale-layaways-file returns-file report-file.
            
            goback.
 
@@ -140,6 +198,7 @@
            perform 20-process-lines.
            perform 30-split-files.
            perform 35-count.
+           perform 37-calculate-totals.
            perform 50-calculate-percentage.
 
            read input-file at end move 'Y' to ws-eof-flag.
@@ -147,40 +206,73 @@
        
        20-process-lines.
            move il-transaction-code to ws-transaction-code.
+
        
        30-split-files.
-           if (ws-transaction-code = ws-sale-transaction-code OR
-               ws-transaction-code = ws-layaway-transaction-code) then
-               add 1 to ws-total-sale-layaways
+           if (ws-transaction-code = ws-sale-code OR
+               ws-transaction-code = ws-layaway-code) then
                write sale-layaways-line from input-line
-           else if (ws-transaction-code
-                       = ws-returns-transaction-code) then
-                add 1 to ws-total-returns
-               write returns-line from input-line
+           else if (ws-transaction-code = ws-return-code) then
+                write returns-line from input-line
+                end-if
            end-if.
 
        35-count.
-           if (ws-transaction-code = ws-sale-transaction-code)
+           if (ws-transaction-code = ws-sale-code)
                add 1 to ws-total-sales
-           else if(ws-transaction-code = ws-layaway-transaction-code)
-               add 1 to ws-total-layaways
+               add 1 to ws-total-sale-layaways
+               add il-transaction-amount to ws-sales-amount
+           else if(ws-transaction-code = ws-layaway-code) then
+                    add 1 to ws-total-layaways
+                    add 1 to ws-total-sale-layaways
+                    add il-transaction-amount to ws-layaways-amount
+                else if(ws-transaction-code = ws-return-code) then
+                         add 1 to ws-total-returns
+                         add il-transaction-amount to ws-returns-amount
+                     end-if
+                end-if
            end-if.
 
+
+       37-calculate-totals.
+           
            add ws-total-returns to ws-total-sale-layaways giving
-             ws-grand-total.
+               ws-total-transactions.
+           add ws-layaways-amount ws-sales-amount giving 
+               ws-total-amount.
+
+           subtract ws-returns-amount from ws-total-amount giving
+               ws-grand-total.
+
+           move ws-total-amount to ws-total-amount-output.
+           move ws-grand-total to ws-grand-total-output.
+           move ws-returns-amount to ws-returns-amount-output.
+           move ws-layaways-amount to ws-layaways-amount-output.
+           move ws-sales-amount to ws-sales-amount-output.
+
        40-output-totals.
-           write sale-layaways-line from ws-total-sale-layaways-detail
+           write report-line from ws-total-sale-layaways-detail
+           write report-line from ws-total-sale-detail.
+           write report-line from ws-total-layaways-detail.
+           write report-line from ws-total-returns-detail.
+           write report-line from ws-transaction-total-detail.
+
+           write report-line               
+               from ws-total-amount-detail after advancing 1 line.
+           write report-line
+               from ws-total-sale-amount-detail.
+           write report-line
+               from ws-total-layaways-amount-detail.
+           write report-line
+               from ws-total-return-amount-detail.
+           write report-line
+               from ws-grand-total-detail.
+
+
+           write report-line from ws-layaways-percentage-detail
                after advancing 1 line.
-           write sale-layaways-line from ws-total-sale-detail.
-           write sale-layaways-line from ws-total-layaways-detail.
-           write returns-line from ws-total-returns-detail
-               after advancing 1 line.
-           write sale-layaways-line from ws-grand-total-detail.
-           write returns-line from ws-grand-total-detail.
-           write sale-layaways-line from ws-layaways-percentage-detail
-               after advancing 1 line.
-           write sale-layaways-line from ws-sales-percentage-detail.
-          
+           write report-line from ws-sales-percentage-detail.
+
        50-calculate-percentage.
            compute ws-layaways-percentage rounded = 
                ws-total-layaways / ws-total-sale-layaways.
--- EDITS/SLProcessing/SLProcessingProgram.cbl
+++ EDITS/SLProcessing/SLProcessingProgram.cbl
@@ -1,7 +1,7 @@
        identification division.
        program-id. SLProcessingProgram.
        author. Joree Miranda, Kyle Bayer, Ashante Smith
-       date-written. 2018-04-06
+       date-written. 2018-04-19
       *This is our 3rd program for our MAFD 4201 Final Project. It will
       *output all of the sales and layaways transaction into a detailed
       *report.
@@ -18,7 +18,7 @@
 
       * output file declarations
            select output-file
-           assign to "../../../data/report.out"
+           assign to "../../../data/Prog3Report.out"
            organization is line sequential.
 
        data division.
@@ -63,7 +63,7 @@
        01 ws-heading.
            05 filler                   pic x(11)
                value "TRANSACTION".
-           05 filler                   pic x(3)
+           05 filler                   pic x(4)
                value spaces.
            05 filler                   pic x(6)
                value "AMOUNT".
@@ -71,7 +71,7 @@
                value spaces.
            05 filler                   pic x(3)
                value "TAX".
-           05 filler                   pic x(5)
+           05 filler                   pic x(6)
                value spaces.
            05 filler                   pic x(12)
                value "PAYMENT TYPE".
@@ -100,25 +100,22 @@
            05 filler                   pic x(41)
                value "-----------------------------------------".
 
-
        01 ws-report-output.
+           05 ws-transaction-output    pic x(7)
+               value spaces.
            05 filler                   pic x(5)
                value spaces.
-           05 ws-transaction-code      pic X
-               value spaces.
-           05 filler                   pic x(6)
-               value spaces.
-           05 ws-transaction-amount    pic $zzzz9.99
+           05 ws-transaction-amount    pic $zz,zz9.99
                value 0.
            05 filler                   pic x(3)
                value spaces.
-           05 ws-tax-output            pic $zzzz9.99
+           05 ws-tax-output            pic $zz,zz9.99
                value 0.
-           05 filler                   pic x(7)
+           05 filler                   pic x(5)
                value spaces.
-           05 ws-payment-type          pic XX
+           05 ws-payment-output        pic X(6)
                value spaces.
-           05 filler                   pic x(10)
+           05 filler                   pic x(8)
                value spaces.
            05 ws-store-number          pic XX
                value spaces.
@@ -131,25 +128,20 @@
            05 ws-sku-code              pic X(15)
                value spaces.
 
+
        01 ws-eof-flag                  pic X
            value 'N'.
 
        01 ws-total-sale-detail.
-           05 filler                   pic x(25)
-               value "TOTAL SALES: ".
+           05 filler                   pic x(24)
+               value "SALE TRANSACTIONS: ".
            05 ws-total-sales           pic 9(2)
                value 0.
 
        01 ws-total-layaways-detail.
-           05 filler                   pic x(25)
-               value "TOTAL LAYAWAYS: ".
+           05 filler                   pic x(24)
+               value "LAYAWAY TRANSACTIONS: ".
            05 ws-total-layaways        pic 9(2)
-               value 0.
-
-       01 ws-total-sale-layaways-detail.
-           05 filler                   pic x(25)
-               value "TOTAL SALES & LAYAWAYS: ".
-           05 ws-total-sale-layaways   pic 9(3)
                value 0.
 
        01 ws-tax                       pic 9999v99
@@ -167,8 +159,26 @@
        01 ws-total-tax                 pic 9999v99
            value 0.
 
+       01 ws-total-amount              pic 9(6)v99
+           value 0.
+
+       01 ws-layaways-amount           pic 9(6)v99
+           value 0.
+
+       01 ws-sales-amount              pic 9(6)v99
+           value 0.
+
+       01 ws-total-sale-layaways       pic 999
+           value 0.
+
+       01 ws-payment-type              pic x(2)
+           value spaces.
+
+       01 ws-transaction-code          pic X
+           value spaces.
+
        01 ws-cash-percentage-detail.
-           05 filler                   pic x(25)
+           05 filler                   pic x(24)
                value "CASH PERCENTAGE: ".
            05 ws-cash-percentage       pic V99
                value 0.
@@ -176,7 +186,7 @@
                value "%".
 
        01 ws-credit-percentage-detail.
-           05 filler                   pic x(25)
+           05 filler                   pic x(24)
                value "CREDIT PERCENTAGE: ".
            05 ws-credit-percentage     pic V99
                value 0.
@@ -184,7 +194,7 @@
                value "%".
 
        01 ws-debit-percentage-detail.
-           05 filler                   pic x(25)
+           05 filler                   pic x(24)
                value "DEBIT PERCENTAGE: ".
            05 ws-debit-percentage      pic V99
                value 0.
@@ -192,10 +202,30 @@
                value "%".
 
        01 ws-total-tax-detail.
-           05 filler                   pic x(25)
+           05 filler                   pic x(24)
                value "TOTAL TAX: ".
-           05 ws-total-tax-output      pic $9999.99
+           05 ws-total-tax-output      pic $z9,999.99
                value 0.
+
+       01 ws-total-layaways-amount-detail.
+           05 filler                      pic x(24)
+               value "TOTAL LAYAWAYS AMOUNT: ".
+           05 ws-layaways-amount-output   pic $z9,999.99
+               value 0.
+
+       01 ws-total-sale-amount-detail.
+           05 filler                      pic x(24)
+               value "TOTAL SALES AMOUNT: ".
+           05 ws-sales-amount-output      pic $99,999.99
+               value 0.
+
+       01 ws-total-amount-detail.
+           05 filler                      pic x(24)
+               value "TOTAL AMOUNT: ".
+           05 ws-total-amount-output      pic $99,999.99
+               value 0.
+
+
 
        77 ws-cash-code                 pic xx
            value "CA".
@@ -220,6 +250,12 @@
 
        77 ws-tax-percent               pic 9v99
            value 0.13.
+
+       77 ws-sale-code                 pic x
+           value 'S'.
+
+       77 ws-layaway-code              pic x
+           value "L".
 
        procedure division.
            open input input-file.
@@ -250,10 +286,13 @@
            
        00-main.
            perform 10-process-lines.
+           perform 11-format-payment.
+           perform 12-format-transaction-code.
            perform 15-calculate-tax.
            perform 20-write-records.
            perform 30-count.
-           perform 35-calculate-percentage.
+           perform 32-calculate-percentage.
+           perform 37-calculate-totals.
 
            read input-file at end move 'Y' to ws-eof-flag.
 
@@ -265,24 +304,46 @@
            move il-invoice-number to ws-invoice-number.
            move il-sku-code to ws-sku-code.
 
+       11-format-payment.
+           if (ws-payment-type = ws-cash-code) then
+               move "CASH" to ws-payment-output
+           else if(ws-payment-type = ws-credit-code) then
+                    move "CREDIT" to ws-payment-output
+                else if(ws-payment-type = ws-debit-code) then
+                         move "DEBIT" to ws-payment-output
+                     end-if
+                end-if
+           end-if.
+
+       12-format-transaction-code.
+           if (ws-transaction-code = ws-sale-code) then
+               move "SALE" to ws-transaction-output
+           else if (ws-transaction-code = ws-layaway-code) then
+                    move "LAYAWAY" to ws-transaction-output
+                end-if
+           end-if.
+
        15-calculate-tax.
            compute ws-tax rounded = 
                il-transaction-amount * ws-tax-percent.
 
            move ws-tax to ws-tax-output.
 
-           add ws-tax to ws-total-tax rounded.
 
        20-write-records.
-           write print-line from ws-report-output.
+           write print-line from ws-report-output
+               after advancing 1 line.
 
        30-count.
            if (ws-transaction-code = ws-sale-transaction-code)
                add 1 to ws-total-sales
                add 1 to ws-total-sale-layaways
+               add il-transaction-amount to ws-sales-amount
            else if(ws-transaction-code = ws-layaway-transaction-code)
                add 1 to ws-total-layaways
                add 1 to ws-total-sale-layaways
+               add il-transaction-amount to ws-layaways-amount
+               end-if
            end-if.
 
            if (ws-payment-type = ws-cash-code) then
@@ -295,7 +356,7 @@
                 end-if
            end-if.
 
-       35-calculate-percentage.
+       32-calculate-percentage.
            compute ws-cash-percentage rounded =
                ws-total-cash / ws-total-sale-layaways.
 
@@ -305,21 +366,40 @@
            compute ws-debit-percentage rounded =
                ws-total-debit / ws-total-sale-layaways.
 
+
+
+       37-calculate-totals.
+           
+           add ws-layaways-amount ws-sales-amount giving 
+               ws-total-amount.
+           
+           multiply ws-total-amount by ws-tax-percent giving
+               ws-total-tax rounded.
+
+           move ws-total-tax to ws-total-tax-output.
+           move ws-total-amount to ws-total-amount-output.
+           move ws-layaways-amount to ws-layaways-amount-output.
+           move ws-sales-amount to ws-sales-amount-output.
+           
+
+
+
        40-output-totals.
-           write print-line from ws-total-sale-layaways-detail
-               after advancing 1 line.
-           write print-line from ws-total-layaways-detail.
+           write print-line from ws-total-layaways-detail
+               after advancing 2 lines.
            write print-line from ws-total-sale-detail.
+
            write print-line from ws-cash-percentage-detail
                after advancing 1 line.
            write print-line from ws-credit-percentage-detail.
            write print-line from ws-debit-percentage-detail.
-           move ws-total-tax to ws-total-tax-output.
+
+           write print-line               
+               from ws-total-amount-detail after advancing 1 line.
+           write print-line
+               from ws-total-sale-amount-detail.
+           write print-line
+               from ws-total-layaways-amount-detail.
            write print-line from ws-total-tax-detail
-               after advancing 1 line.
-
-
-
-       
 
        end program SLProcessingProgram.
